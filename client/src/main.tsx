8b7720b7c8a0a7ff7b2c263ef88e1ed83d9ffc92
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-13 16:32:15 +0100

    Ajout des configuration de base côté client pour que cela soit un minimum fonctionnel

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HelmetProvider } from 'react-helmet-async';
+
+/*Les importation de page*/
+import HomePage from './pages/HomePage.tsx';
+
+const router = createBrowserRouter([
+    {
+     element: <App />,
+     children: [
+       {
+         path: "/",
+         element: <HomePage />,
+       },
+      ]
+    },
+  ]);
+
+const rootElement = document.getElementById("root");
+if (rootElement == null) {
+  throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
+}
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+     <HelmetProvider>
+       <RouterProvider router={router}/>
+     </HelmetProvider>
 )
